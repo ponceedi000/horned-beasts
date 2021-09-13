56cc9d51cedcf049653d63d2939d9828f5a21def
Ediberto Ponce <ediberto01@gmail.com>
2021-09-13 14:34:44 -0700

    created repo from command line

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css';
+import { Component } from 'react';
+import Header from './Header.js';
+
+function App() {
+  return  (
+    <div>
+      <Header />
+      <Main message={'Hi there'} />
+      <Footer />
+    </div>
+  );
+}
+
+class Header extends Component {
+  render() {
+    return <h1>I am a header</h1>
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return <h1>I am a footer</h1>
+  }
+}
+
+class Main extends Component {
+  //needs to know the title pass in somehow....
+  render() {
+    return (
+    <>
+    <h1>I am a Mai my message is {this.props.message} </h1>
+    <HornedBeast title={'Rhino'}/>
+    </>
+    )
+  }
+}
+
+class HornedBeast extends Component {
+  render() {
+    return <h1>I am a horned beast with the title of {'this.props.title'}</h1>
+  }
+}
+ 
+export default App;
