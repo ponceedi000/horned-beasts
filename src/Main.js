46c63005c3cf3a46d6862e36df50537b172cac73
Ediberto Ponce <ediberto01@gmail.com>
2021-09-14 09:36:46 -0700

    Better start for Main.js file

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,31 @@
 import { Component } from 'react';
+//import itchy from './images/itchy'
+//import images here
+
 
 class Main extends Component {
-
     render() {
-        return <h1>I am a header in the main</h1>
+        
+        return( 
+        <> 
+        <HorenBeast title={} imgURL={} description={''}/>
+        <HorenBeast title={} imgURL={} description={''}/>
+        </>
+            )
     }
-
 }
+
+class HornedBeast extends Component {
+    render() {
+        return (
+            <>
+            <h2>This horned beast is known as {this.props.title}</h2>
+            <img src={this.props.imgURL} alt='hornedBeast_img' title={this.props.title} />
+            <p>{this.props.description</p>
+            </>
+        )
+    }
+}
+
 
 export default Main;
