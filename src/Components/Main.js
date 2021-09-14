27674be07b8eaa821db7fa99cca21ca727616fbe
Ediberto Ponce <ediberto01@gmail.com>
2021-09-14 16:50:42 -0700

    Lab01 completed

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,14 +1,14 @@
 import { Component } from 'react';
-import markhor from './img/markhor.jpg';
-import saiga from './img/saiga.jpg';
+import markhor from '../img/markhor.jpg';
+import saiga from '../img/saiga.jpg';
 
 
 class Main extends Component {
     render() {       
         return( 
         <> 
-        <HorenBeast title={'Markhor'} imgURL={'markhor'} description={'Lives in the mountains of central Asia'}/>
-        <HorenBeast title={'Saiga'} imgURL={'saiga'} description={'Native to the dusty steppes of Kazakhstan, Russia'}/>
+        <HornedBeast title={'Markhor'} imgURL={markhor} description={'Lives in the mountains of central Asia'}/>
+        <HornedBeast title={'Saiga'} imgURL={saiga} description={'Native to the dusty steppes of Kazakhstan, Russia'}/>
         </>
          )
     }
